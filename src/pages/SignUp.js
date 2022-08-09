aa6393868fcf91cd8128f14cdf941d5723ccb707
GunHyuk Lee <bood00142@gmail.com>
2022-08-10 05:24:53 +0900

    :passport_control: Auth part develop

---
+++ src/pages/SignUp.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Auth from "../components/auth/Auth.js";
+
+const SignUp = () => {
+    const [email, setEmail] = useState();
+
+    return (
+        <div class="App-header">
+            <h1>회원가입</h1>
+            <input type="email" id="email" />
+            <input type="password" id="password" />
+            <Link to="/">
+                <button>제출</button>
+            </Link>
+        </div>
+    );
+};
+
+export default SignUp;
